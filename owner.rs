601f6c18258182dc82b3599589c76af8c00de6b8
manisha132 <51741605+manisha132@users.noreply.github.com>
2021-10-28 18:17:12 +0530

    Add files via upload

---
+++ owner.rs
@@ -0,0 +1,9 @@
+fn main()
+{
+    let mut s = String::from("manisha");
+
+    s.push_str(", saroj!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+}
