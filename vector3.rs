931f56056989a8f601750dfe7292b2623a089100
manisha132 <51741605+manisha132@users.noreply.github.com>
2021-10-30 16:40:59 +0530

    Add files via upload

---
+++ vector3.rs
@@ -0,0 +1,12 @@
+fn main() {
+ 
+    let v = vec!['G','E','E','K','S'];
+ 
+    // here index is the non negative value which is smaller than the size of the vector
+    let index: usize = 3;
+ 
+    //getting value at given index value
+    let ch: char = v[index];
+ 
+    print!("{ }\n",ch);
+}
